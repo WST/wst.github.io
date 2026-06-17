fe8bfb56dee3060b8e0fa2649ced42b1c5e9fb1f
WST <ilya@averkov.net>
2026-06-17 22:51:30 +0400

    Update copyright year in documentation from 2025 to 2026 across multiple files; adjust build info configuration accordingly.

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Knowledge Base'
-copyright = '2025, Ilya Averkov'
+copyright = '2026, Ilya Averkov'
 author = 'Ilya Averkov'
 
 # -- General configuration ---------------------------------------------------
